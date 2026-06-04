2edcc80133ea49e3f43e4bf5a8a14b413d3f1d4d
Graja <houssemeddine.graja@insat.ucar.tn>
2026-06-04 23:51:17 +0100

    another momentary delete

--- translator.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("tencent/HY-MT1.5-1.8B")
-model = AutoModelForCausalLM.from_pretrained("tencent/HY-MT1.5-1.8B")
