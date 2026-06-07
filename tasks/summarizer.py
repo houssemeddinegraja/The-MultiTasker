8d026541049dd57c9c9fa3372ec93113ec4b97fc
Graja <houssemeddine.graja@insat.ucar.tn>
2026-06-07 16:32:48 +0100

    Enhance summarization function with length options
    Added functionality to summarize text based on dropdown choice for summary length.

--- tasks/summarizer.py
+++ tasks/summarizer.py
@@ -2,12 +2,16 @@ from transformers import pipeline
 
 summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 
-def summarize_text(text):
-    summary = summarizer(
-        text,
-        max_length=128,
-        min_length=64,
-        do_sample=False,
-        clean_up_tokenization_spaces=True
-    )
-    return summary
+def summarize(text, dropdown_choice):
+    if dropdown_choice == "Short":
+        max = 45
+        min = 15
+    elif dropdown_choice == "Detailed":
+        max = 160
+        min = 80
+    else:
+        max = 100
+        min = 50
+
+    summary = summarizer(text, max_length=max, min_length=min, do_sample=False, clean_up_tokenization_spaces=True)
+    return summary[0]['summary_text']
